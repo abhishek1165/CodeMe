facb49d1f444a5534248394da6923466622c8660
abhishek1165 <abhishekkachha12345@gmail.com>
2025-08-21 22:43:17 +0530

    add editor page and home landing page done some css

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import Home from './component/Home';
+import { Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import EditorPage from './components/EditorPage';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <Home/>
+    <>
+    <div>
+      <Toaster  position='top-center'></Toaster>
+    </div>
+    <Routes>
+     <Route path='/' element={ <Home /> } />
+     <Route path='/editor/:roomId' element={ <EditorPage /> } />
+    </Routes>
+    </>
   );
 }
 
